d7dc508e32330d3ec5ac9e0c4bf05e8aa7831526
mingzhe4 <112341928+mingzhe4@users.noreply.github.com>
2022-09-18 13:58:28 -0400

    segr

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -1,3 +1,10 @@
-function glazingChange(element){
+function glazingChange(element) {
+
+
+    let element1 = document.querySelector("#keepOriginal")
+    console.log(element1)
+    // get value of selected glazing option
     const priceChange = element.value;
-}
+    
+  // add your code to do update the price ...
+  }
