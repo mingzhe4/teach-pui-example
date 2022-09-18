9e9c3931a4a391f210c0f0996cb05f8fb117461f
mingzhe4 <112341928+mingzhe4@users.noreply.github.com>
2022-09-18 13:07:57 -0400

    we

--- solution-hw3/script.js
+++ solution-hw3/script.js
@@ -1,1 +1,11 @@
-console.log("print")
+function glazingChange(element) {
+
+
+    let element = document.querySelector("#glazingOptions")
+    console.log(element)
+    // get value of selected glazing option
+    const priceChange = element.value;
+    
+  // add your code to do update the price ...
+  }
+  
