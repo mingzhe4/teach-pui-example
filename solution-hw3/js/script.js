b02974fd62f723f5c48b91067d7fecf9f08c9382
mingzhe4 <112341928+mingzhe4@users.noreply.github.com>
2022-09-19 10:18:51 -0400

    adw

--- solution-hw3/js/script.js
+++ solution-hw3/js/script.js
@@ -6,24 +6,24 @@ const addPrice2 = {
     packSize:[1,3,5,10]
 }
 
-const element1 = document.querySelector('#glazingOptions')
-const element2 = document.querySelector('#size')
+const elementGlaze = document.querySelector('#glazingOptions')
+const elementSize = document.querySelector('#size')
 
 for(i=0;i<4;i++){
-    element1.options[i].value=addPrice1.glazing[i];
+    elementGlaze.options[i].value=addPrice1.glazing[i];
 }
 
 for(j=0;j<4;j++){
-    element2.options[j].value=addPrice2.packSize[j];
+    elementSize.options[j].value=addPrice2.packSize[j];
 }
 
 function updatePrice(){
-    let showPrice  = document.querySelector("#price")
+    showPrice  = document.querySelector("#price")
     showPrice.textContent = '$'+finalPrice;
 }
 
 function glazingChange() {
-    finalPrice = (Number(Number(element2.value)*(Number(2.49+Number(element1.value))))).toFixed(2);   
+    finalPrice = (Number(Number(elementSize.value)*(Number(2.49+Number(elementGlaze.value))))).toFixed(2);   
     updatePrice()
   }
 
